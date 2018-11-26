e285344d89da99e8097a9950297acbc735e956fe
Jonnie Castillo <jonnie164@gmail.com>
2018-11-26 11:27:39 -0600

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,39 @@ export class InfoPaginaService {
 
 	info: InfoPagina = {};
 	cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
 
   //console.log ('Info');
+   this.cargarInfo();
+this.cargarEquipo();
+    
+   }
 
-      this.http.get('assets/data/data-pagina.json')
+
+    private cargarInfo(){
+
+  this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
-         	    this.cargada = true;
-      	    this.info = resp;
+               this.cargada = true;
+            this.info = resp;
+      });
+
+    }
+
+    private cargarEquipo()
+    {
+       this.http.get('https://angular-html-9e84c.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[])  => {
+              
+            this.equipo = resp;
             console.log(resp);
       });
-   }
+
+    }
+
+
 }
  
 
